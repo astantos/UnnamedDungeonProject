008e0265a344c34b739fbbd9a2af4ea1a1d7cdc2
astantos <jimxu9000plus@gmail.com>
2022-09-27 19:36:03 +1000

    Added type weighting to connection points so that certain types of rooms would be more likely to appear at certain connections. Currently generation still takes a long time - I believe this issue would be resolved with a greater variety of room shapes and sizes.

--- Assets/Scripts/ProceduralMapGeneration/GridlessGeneration/ConnectionPoint.cs
+++ Assets/Scripts/ProceduralMapGeneration/GridlessGeneration/ConnectionPoint.cs
@@ -1,16 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ConnectionPoint : MonoBehaviour
 {
+    [Serializable]
+    public struct ConnectionType
+    {
+        public Room.RoomType Type;
+        public int Weight;
+    }
+
     public RoomDictionary RoomDictionary;
 
     public GameObject EnabledObject;
     public GameObject DisabledObject;
 
     public bool FreeConnection;
-    public List<Room.RoomType> ValidRoomTypes;
+    public List<ConnectionType> ValidRoomTypes;
 
     protected Coroutine spawnRoutine;
 
@@ -57,54 +66,70 @@ public class ConnectionPoint : MonoBehaviour
         //Debug.Log($"[ CONNECTION POINT ] {gameObject.name}: Attempting Placement");
         PlacementValid = false;
 
-        List<Room> validRooms = GetValidRooms();
+        List<ConnectionType> validConnections = new List<ConnectionType>(ValidRoomTypes);
+        ConnectionType connection;
 
-        while(validRooms.Count > 0)
+        while(validConnections.Count > 0)
         {
-            int roomIndex = Random.Range(0, validRooms.Count);
-            Room room = GameObject.Instantiate(validRooms[roomIndex]);
-            room.Initialize(generator);
-            //room.gameObject.name = $"{room.gameObject.name}_Current";
-            //Debug.Log($"[ CONNECTION POINT ] {gameObject.name}: Attempting to Place {room.gameObject.name}");
-            yield return room.TryPlace(this);
-            if (room.PlacementValid)
+            connection = GetRandomConnection(validConnections);
+            Room.RoomType roomType = connection.Type;
+
+            List<Room> validRooms = GetValidRooms(roomType);
+
+            while(validRooms.Count > 0)
             {
-                //Debug.Log($"[ CONNECTION POINT ] {gameObject.name}: Placement Successful");
-                PlacementValid = true;
-                generator.AddRoom(room);
-                //room.gameObject.name = $"{room.gameObject.name}_PLACED";
+                int roomIndex = Random.Range(0, validRooms.Count);
+                Room room = GameObject.Instantiate(validRooms[roomIndex]);
+                room.Initialize(generator);
+                //room.gameObject.name = $"{room.gameObject.name}_Current";
+                //Debug.Log($"[ CONNECTION POINT ] {gameObject.name}: Attempting to Place {room.gameObject.name}");
+                yield return room.TryPlace(this);
+                if (room.PlacementValid)
+                {
+                    //Debug.Log($"[ CONNECTION POINT ] {gameObject.name}: Placement Successful");
+                    PlacementValid = true;
+                    generator.AddRoom(room);
+                    //room.gameObject.name = $"{room.gameObject.name}_PLACED";
+                    break;
+                }
+                else
+                {
+                    // Selected Room Cannot Be Placed
+                    //Debug.Log($"[ CONNECTION POINT ] {gameObject.name}: Placement Not Successful, removing {room.gameObject.name} as valid");
+                    room.StopAllCoroutines();
+                    //room.gameObject.name = $"{room.gameObject.name}_DESTROYED";
+                    GameObject.Destroy(room.gameObject);
+                    validRooms.RemoveAt(roomIndex);
+                }
+            }
+
+            if (PlacementValid)
+            {
+                EnableConnector(false);
                 break;
             }
             else
             {
-                // Selected Room Cannot Be Placed
-                //Debug.Log($"[ CONNECTION POINT ] {gameObject.name}: Placement Not Successful, removing {room.gameObject.name} as valid");
-                room.StopAllCoroutines();
-                //room.gameObject.name = $"{room.gameObject.name}_DESTROYED";
-                GameObject.Destroy(room.gameObject);
-                validRooms.RemoveAt(roomIndex);
+                //Debug.Log($"[ CONNECTION POINT ] {gameObject.name}: No Valid Placements, disabling connector");
+                validConnections.Remove(connection);
             }
         }
 
-        if (PlacementValid)
+
+        if (PlacementValid == false)
         {
-            EnableConnector(false);
-        }
-        else
-        {
-            //Debug.Log($"[ CONNECTION POINT ] {gameObject.name}: No Valid Placements, disabling connector");
             EnableConnector(false);
         }
     }
 
     #region UtilityFunctions
-    protected List<Room> GetValidRooms()
+    protected List<Room> GetValidRooms(Room.RoomType roomType)
     {
         List<Room> validRooms = new List<Room>();
 
         for (int roomList = 0; roomList< RoomDictionary.RoomLists.Count; roomList++)
         {
-            if (ValidRoomTypes.Contains(RoomDictionary.RoomLists[roomList].RoomType))
+            if (RoomDictionary.RoomLists[roomList].RoomType.Equals(roomType))
             {
                 for (int index = 0; index < RoomDictionary.RoomLists[roomList].Rooms.Count; index++)
                 {
@@ -114,6 +139,31 @@ public class ConnectionPoint : MonoBehaviour
         }
         return validRooms;
     }
+
+    protected ConnectionType GetRandomConnection(List<ConnectionType> validTypes)
+    {
+        int totalWeight = 0;
+        for (int index = 0; index < validTypes.Count; index++)
+        {
+            totalWeight += validTypes[index].Weight;
+        }
+
+        if (totalWeight > 0)
+        {
+            int randomTarget = Random.Range(0, totalWeight);
+            int current = 0;
+            for (int index = 0; index < validTypes.Count; index++)
+            {
+                current += validTypes[index].Weight;
+                if (current >= randomTarget)
+                    return validTypes[index];
+            }
+        }
+
+        Debug.LogWarning("Returning first as default");
+        return validTypes[0];
+    }
+
     #endregion
 
 }
