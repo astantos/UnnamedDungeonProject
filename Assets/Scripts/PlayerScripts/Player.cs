2c328fef6fc65231c6d8284f5bf1ff1db7b13c1d
astantos <jimxu9000plus@gmail.com>
2022-10-13 19:53:46 +1100

    Added Player and Player Collision, Renamed PlayerController to just Player due to a Unity Bug.

--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public class PlayerController : NetworkBehaviour
+public class Player : NetworkBehaviour
 {
     public Camera MainCamera;
     public BasicMovement Movement;
